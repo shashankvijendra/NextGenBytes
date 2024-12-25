250d508918d31bca546a04bdd0ea88f72b88f22e
shashank <shashankvijendra@gmail.com>
2024-12-25 08:20:53 +0530

    go with array

--- GoEngine/GoFlow/my-go-project/array.go
+++ GoEngine/GoFlow/my-go-project/array.go
@@ -30,6 +30,39 @@ func arrayWithString() {
 	fmt.Println(cars)
 }
 
+func arrayIndex() {
+	prices := [3]int{10, 20, 30}
+	fmt.Println("Array index")
+	fmt.Println(prices[0])
+	fmt.Println(prices[2])
+}
+
+func arrayInitialization() {
+	arr1 := [5]int{}              //not initialized
+	arr2 := [5]int{1, 2}          //partially initialized
+	arr3 := [5]int{1, 2, 3, 4, 5} //fully initialized
+
+	fmt.Println("Array initialization")
+	fmt.Println(arr1)
+	fmt.Println(arr2)
+	fmt.Println(arr3)
+}
+
+func arrayInitializeOnlySpecificElements() {
+	arr1 := [5]int{1: 10, 2: 40, 4: 1}
+
+	fmt.Println("Array initialization only specific elements")
+	fmt.Println(arr1)
+}
+
+func arrayLength() {
+	arr1 := [4]string{"Volvo", "BMW", "Ford", "Mazda"}
+	arr2 := [...]int{1, 2, 3, 4, 5, 6}
+	fmt.Println("Array length")
+	fmt.Println(len(arr1))
+	fmt.Println(len(arr2))
+}
+
 // main is the entry point of the program
 func main() {
 	// Demonstrate array with fixed length
@@ -38,4 +71,12 @@ func main() {
 	defineDynamicLengthArrays()
 	// Demonstrate array with string elements
 	arrayWithString()
+	//Array with index
+	arrayIndex()
+	//Array initialization
+	arrayInitialization()
+	//Array initialize only specific elements
+	arrayInitializeOnlySpecificElements()
+	// Find the len of the array
+	arrayLength()
 }
